6c9e59950129007a412443b00b2f406dfe61e793
Vishnu Ajith <27vishnu07@gmail.com>
2023-06-19 02:30:47 +0530

    bug fix

--- App/urls.py
+++ App/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.DetailView.as_view(), name="post_detail"),
     path('book.html', views.NavB.as_view(), name='book'),
-    path('film.html', views.NavF.as_view(), name='book'),
-    path('general.html', views.NavG.as_view(), name='book'),
+    path('film.html', views.NavF.as_view(), name='film'),
+    path('general.html', views.NavG.as_view(), name='general'),
 ]
